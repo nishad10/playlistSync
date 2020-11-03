38b9b6ce8f133be0a6812080faf973c1cb01b395
Nishad Aherrao <nishad.aherrao10@gmail.com>
2020-11-02 18:49:33 -0600

    Add songs/tracks data to playlist

--- services/appleMusicUtils.js
+++ services/appleMusicUtils.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const axiosApple = axios.create({
-  baseURL: 'https://amp-api.music.apple.com/v1/me/library/playlist-folders/p.playlistsroot/children',
+  baseURL: 'https://amp-api.music.apple.com',
   timeout: 1000,
    headers: { 
     'media-user-token': process.env.MEDIATOKEN, 
@@ -10,20 +10,34 @@ const axiosApple = axios.create({
   }
 });
 
-const getPlaylistData = () => {
-    return axiosApple.get().then((response)=> {
+const getPlaylistData = async () => {
+    return await axiosApple.get('/v1/me/library/playlist-folders/p.playlistsroot/children').then((response)=> {
         data = response.data.data
         const playlistFinal = data.map(playlist => {
-            return ({
+            return ([playlist.id,{
                 'id': playlist.id,
                 'url': playlist.href,
                 'name': playlist.attributes.name,
-            })
+            }])
         })
-        return playlistFinal
+        const playlistData = Object.fromEntries(playlistFinal)
+        return playlistData
     }).catch((error) => {
       console.log(error)
   });
 }
 
-module.exports = {getPlaylistData}
+const addMusicData = async (playlists) => {
+  const requests = []
+      for (const [pid, playlist] of Object.entries(playlists)) {
+      requests.push(axiosApple.get(playlist.url+'?include=tracks&include%5Blibrary-playlists%5D=catalog%2Ctracks'))
+    }
+    const responses = await Promise.all(requests)
+    responses.forEach(response => {
+        const musicAttributes = response.data.data[0].relationships.tracks.data
+        playlists['songData'] = musicAttributes
+    })
+    return playlists
+}
+
+module.exports = {getPlaylistData, addMusicData}
--- services/index.js
+++ services/index.js
@@ -1,7 +1,10 @@
 const appleMusicUtils = require('./appleMusicUtils')
 
-const sync = () => {
+const sync = async () => {
     console.log('Syncing')
+    const playlists = await appleMusicUtils.getPlaylistData()
+    const playlistsWithMusic = await appleMusicUtils.addMusicData(playlists)
+    console.log(playlistsWithMusic)
 }
 
 module.exports = { sync }
